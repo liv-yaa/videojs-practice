91ea406899807f3562180ba3d467bb350a3c4301
liv <olivia.f.smith@gmail.com>
2020-10-11 10:42:33 -0700

    adding sc

---
+++ index.js
@@ -0,0 +1,8 @@
+// index.js
+// Following this class to start an express server at `http://localhost:3000/`
+// Uses two libraries to create an Express app and start it at the specified port
+// https://www.coursera.org/learn/server-side-nodejs/supplement/DdJt7/exercise-instructions-introduction-to-express
+
+const express = require('express')
+const http = require('http');
+const morgan = require('morgan'); // used for logging purposes
